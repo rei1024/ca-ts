ea900ae90a375686813a493a6295bd7235783017
rei1024 <satopen1729@gmail.com>
2024-07-09 13:27:55 +0900

    feat(rle): update version

--- rle/RLE.ts
+++ rle/RLE.ts
@@ -1,6 +1,6 @@
 /**
  * Run Length Encoded file format
- * 
+ *
  * ### Reference
  * [Run Length Encoded | Life Wiki](https://conwaylife.com/wiki/Run_Length_Encoded)
  */
---
+++ rle/mod.ts
@@ -0,0 +1,11 @@
+/**
+ * Run Length Encoded file format
+ *
+ * ### Reference
+ * [Run Length Encoded | Life Wiki](https://conwaylife.com/wiki/Run_Length_Encoded)
+ * @module
+ */
+
+export type { RLE } from "./RLE.ts";
+export { readRLE } from "./readRLE.ts";
+export { writeRLE } from "./writeRLE.ts";
