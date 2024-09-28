ca412bf24e5a5d4613dded9f33eab90faa7ebf3e
rei1024 <satopen1729@gmail.com>
2024-09-28 21:22:41 +0900

    feat: add example

--- algo/bit/mod.ts
+++ algo/bit/mod.ts
@@ -1,5 +1,24 @@
 /**
  * Conway's Game of Life simulation by bitwise operations.
+ *
+ * @example
+ * ```ts
+import { BitWorld } from "./BitWorld.ts";
+const world = BitWorld.make({ width: 32, height: 32 });
+world.random();
+setInterval(() => {
+  console.clear();
+  console.log("__".repeat(32));
+  console.log(
+    world.getArray().map((row) =>
+      row.map((x) => x === 1 ? "O " : "  ").join("")
+    )
+      .join("\n"),
+  );
+  world.next();
+}, 100);
+ * ```
+ *
  * @module
  */
 export { BitGrid } from "./BitGrid.ts";
