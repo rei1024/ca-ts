37be1e163d26da2821d26616f41e31b6b710f8db
rei1024 <satopen1729@gmail.com>
2025-04-04 00:32:04 +0900

    feat: add module docs and CHANGELOG

--- packages/pattern/mod.ts
+++ packages/pattern/mod.ts
@@ -1,3 +1,19 @@
+/**
+ * This module provides a set of utilities for working with cellular automata (CA) patterns.
+ *
+ * @example
+ * ```ts
+ * import { CACellList } from "@ca-ts/pattern";
+ * const cellList = CACellList.fromCells([
+ *  { position: { x: 0, y: 0 }, state: 1 },
+ *  { position: { x: 1, y: 0 }, state: 2 },
+ *  { position: { x: 2, y: 0 }, state: 3 },
+ * ]);
+ * const array = cellList.to2dArray();
+ * ```
+ *
+ * @module
+ */
 export type { CACell } from "./types.ts";
 export { CACellList } from "./ca-cell-list.ts";
 export { BoundingRect } from "./bounding-rect.ts";
