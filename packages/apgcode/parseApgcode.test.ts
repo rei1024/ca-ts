00ee93a77d662b938fe35dbaedf8ccd8570f4f95
rei1024 <satopen1729@gmail.com>
2025-04-11 22:03:25 +0900

    fix: test

--- packages/apgcode/parseApgcode.test.ts
+++ packages/apgcode/parseApgcode.test.ts
@@ -118,6 +118,12 @@ Deno.test("parseApgcode period error", () => {
   }, ApgcodeParseError);
 });
 
+Deno.test("parseExtendedWechslerFormat Error", () => {
+  assertThrows(() => {
+    parseExtendedWechslerFormat("!");
+  }, ApgcodeParseError);
+});
+
 Deno.test("parseExtendedWechslerFormat Error space", () => {
   assertThrows(() => {
     parseExtendedWechslerFormat("3 3");
