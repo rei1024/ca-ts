2a933948a340d2507bbacc081a38226ad77436ee
rei1024 <satopen1729@gmail.com>
2025-04-13 15:10:34 +0900

    feat: validation

--- packages/apgcode/stringifyApgcode.ts
+++ packages/apgcode/stringifyApgcode.ts
@@ -26,4 +26,21 @@ export function stringifyApgcode(apgcode: Apgcode): string {
 export function stringifyExtendedWechslerFormat(
   cells: { x: number; y: number }[],
 ): string {
+  validate(cells);
+}
+
+function validate(cells: { x: number; y: number }[]) {
+  let prevCell = undefined;
+  for (const cell of cells) {
+    if (!Number.isInteger(cell.x) || !Number.isInteger(cell.y)) {
+      throw new Error("Not an integer");
+    }
+    if (
+      prevCell &&
+      (prevCell.y > cell.y || (prevCell.y === cell.y && prevCell.x > cell.x))
+    ) {
+      throw new Error("Not sorted and unique");
+    }
+    prevCell = cell;
+  }
 }
